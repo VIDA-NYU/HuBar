fd6c64f4d3b664b2f1b7d04ba3050059b005b8ef
Danish Tamboli <danishtamboli123@gmail.com>
2024-06-05 00:22:57 -0400

    feat(Workload-Module): Complete functionality implemented
    Styling changes might be needed, Data Inconsistency handling might need to be refined

--- src/views/IdealView.js
+++ src/views/IdealView.js
@@ -2,21 +2,29 @@ import { get_brushedSubject, get_brushedTrial, get_vidEnd, get_vidStart, set_bru
 import { get_videoPlayer } from './videoPlayerUtils.js';
 import * as d3 from 'd3';
 
-var FlightPhase = null;
-var Task = null;
-var TimeDemand = null;
-var PerceptualOperations = [];
-var CognitiveOperations = [];
-var MotorOperations = [];
-var WorkingMemoryLoad = null;
-var OverallWorkload = null ;
-var ChartImg = null;
+// List of field labels
+const fieldLabels = [
+  ['Total Time Demand','seconds','total-time-demand'],
+  ['Overall Working Memeory Load', 'chunks','overall-working-memeory-load'],
+  ['Overall Workload Rating', '','overall-workload-rating'],
+  ['Procedure Time Demand','seconds','procedure-time-demand'],
+  ['Procedure Working Memory Load','chunks','procedure-working-memmory-load'],
+  ['Procedure Workload','','procedure-workload'],
+];
+
+var ClosestMatch;
+var DefaultContainer;
 var MissionLogData = null;
+var Reset = true;
 
 var PreviousTrialID = null;
 var PreviousSubjectID = null;
 var PreviousFlightPhase = null;
 var PreviousProcedure = null;
+
+window.addEventListener("load", async() => {
+  LoadWorkloadModule();
+})
 
 export async function TestScript() {
     let SubjectID = get_brushedSubject();
@@ -25,21 +33,20 @@ export async function TestScript() {
     if(SubjectID !== null && TrialID !== null && (VideoCurrentTime !== null && VideoCurrentTime > 0)){
         if(PreviousSubjectID !== SubjectID || PreviousTrialID !== TrialID){
             resetToDefaultView();
-            MissionLogData = await fetchMissionLogNData("../data/formatted_mission_log_seconds.json");
+            MissionLogData = await fetchData("../data/formatted_mission_log_seconds.json");
             PreviousSubjectID = SubjectID;
             PreviousTrialID = TrialID;
         }
-        var PhaseAndProcedure = findCurrentPhaseAndProcedure(MissionLogData, SubjectID, TrialID, VideoCurrentTime);
-        if(PhaseAndProcedure){
-          if(PreviousFlightPhase !== PhaseAndProcedure.FlightPhase || PreviousProcedure !== PhaseAndProcedure.Procedure){
-            console.log(PhaseAndProcedure)
-            updateOptimalTaskView();
-            PreviousFlightPhase = PhaseAndProcedure.FlightPhase;
-            PreviousProcedure = PhaseAndProcedure.Procedure;
+        ClosestMatch = findCurrentPhaseAndProcedure(MissionLogData, SubjectID, TrialID, VideoCurrentTime);
+        if(ClosestMatch){
+          if(PreviousFlightPhase !== ClosestMatch.FlightPhase || PreviousProcedure !== ClosestMatch.Procedure){
+            updateOptimalTaskView(ClosestMatch);
+            PreviousFlightPhase = ClosestMatch.FlightPhase;
+            PreviousProcedure = ClosestMatch.Procedure;
           }
         }
     }
-    else{
+    else if(!Reset){
         resetToDefaultView();
     }
 
@@ -50,15 +57,7 @@ export async function TestScript() {
   }
 
   function resetToDefaultView(){
-    FlightPhase = null;
-    Task = null;
-    TimeDemand = null;
-    PerceptualOperations = [];
-    CognitiveOperations = [];
-    MotorOperations = [];
-    WorkingMemoryLoad = null;
-    OverallWorkload = null; 
-    ChartImg = null;
+    ClosestMatch = null;
     MissionLogData =null;
 
     PreviousSubjectID = null;
@@ -66,96 +65,53 @@ export async function TestScript() {
     PreviousFlightPhase = null;
     PreviousProcedure = null;
 
-    const targetDiv = document.getElementById('ideal-log-container-child');
-    if (targetDiv) {
-      targetDiv.innerHTML = '';
-    }
+    LoadDefaultContainer();
+    Reset = true;
   }
 
-  function updateOptimalTaskView(){
-
+  function LoadDefaultContainer(){
     const targetDiv = document.getElementById('ideal-log-container-child');
-
-    if (targetDiv) {
-      // Create the main container div
-      const mainContainer = document.createElement('div');
-      mainContainer.style.display = 'flex';
-      mainContainer.style.flexDirection = 'row';
-      mainContainer.style.flexWrap = 'wrap';
-      mainContainer.style.justifyContent = 'center';
-      mainContainer.style.overflow = 'auto';
-
-      // mainContainer.style.height = '100%';
-      
-      // List of field labels
-      const fieldLabels = [
-        'Flight Stage',
-        'Total Time Demand',
-        'Overall Workload',
-        'Flight Stage',
-        'Time Demand',
-        'Perceptual Operations',
-        'Cognitive Operations',
-        'Motor Operations',
-        'Working Memory Load',
-        'Overall Workload'
-      ];
-      
-      // Create and append the data fields
-      fieldLabels.forEach(label => {
-        const dataField = document.createElement('div');
-        dataField.className = 'ideal-workload-datafield';
-      
-        const innerDiv = document.createElement('div');
-        innerDiv.style.textAlign = 'center';
-        innerDiv.style.display = 'flex';
-        innerDiv.style.flexDirection = 'row';
-        innerDiv.style.margin = 'auto';
-      
-        const valueDiv = document.createElement('div');
-        valueDiv.style.marginTop = 'auto';
-        const valueText = document.createElement('h1');
-        valueText.style.margin = "auto";
-        valueText.textContent = '6'; // Example value, you can set this dynamically
-        valueDiv.appendChild(valueText);
-      
-        const unitDiv = document.createElement('div');
-        unitDiv.style.marginTop = 'auto';
-        const unitText = document.createElement('p');
-        unitText.style.marginBottom = "0px";
-        unitText.textContent = 's'; // Example unit, you can set this dynamically
-        unitDiv.appendChild(unitText);
-      
-        innerDiv.appendChild(valueDiv);
-        innerDiv.appendChild(unitDiv);
-      
-        dataField.appendChild(innerDiv);
-      
-        const strongElement = document.createElement('p');
-        strongElement.style.fontStyle = "italic";
-        strongElement.style.margin = "0px";
-        strongElement.textContent = label;
-        dataField.appendChild(strongElement);
-      
-        mainContainer.appendChild(dataField);
-      });
-
-      // Create the image container
-      const imageContainer = document.createElement('div');
-      imageContainer.style.width = "100%";
-      const imageElement = document.createElement('img');
-      imageElement.src = 'data/images/preflight_1_chartView.png';
-      imageElement.style.width = "inherit";
-      imageContainer.appendChild(imageElement);
-
-      // Clear the existing content
-      targetDiv.innerHTML = '';
-
-      // Append the main container and image container to the target div
-      targetDiv.appendChild(mainContainer);
-      targetDiv.appendChild(imageContainer);
+    if(targetDiv){
+      targetDiv.outerHTML = DefaultContainer;
     }
+    const titleContainer = document.getElementById('workload-module-title');
+    titleContainer.innerHTML = "Workload Module";
 
+    const imageContainer = document.getElementById('work-module-image');
+    imageContainer.src = '';
+  }
+
+  async function updateOptimalTaskView(Match){
+    if(Match.FlightPhase != null || Match.Procedure != null){
+      const containerTitle = document.getElementById('workload-module-title');
+      containerTitle.innerHTML = `Workload Module ${Match.FlightPhase} ${Match.Procedure}`;
+
+      var LookUpJSON = await fetchData("../data/workload_module_lookup_table.json");
+
+      fieldLabels.forEach(label => {
+
+        var DataTextDiv = document.getElementById(label[2]);
+
+        if(label[2] === "total-time-demand"){
+          DataTextDiv.innerText = LookUpJSON[Match.FlightPhase].total_time_demand;
+        }
+        else if(label[2] === "overall-working-memeory-load"){
+          DataTextDiv.innerText = LookUpJSON[Match.FlightPhase].total_working_memory_load;
+
+        }
+        else if(label[2] === "overall-workload-rating"){
+          DataTextDiv.innerText = LookUpJSON[Match.FlightPhase].overall_workload;
+        }
+        else{
+          DataTextDiv.innerText = LookUpJSON[Match.FlightPhase][Match.Procedure][label[2]];
+        }
+      })
+
+      const imageContainer = document.getElementById('work-module-image');
+      imageContainer.src = LookUpJSON[Match.FlightPhase][Match.Procedure].chart_url;
+
+      Reset = false;
+    }
   }
 
 
@@ -183,21 +139,130 @@ export async function TestScript() {
     // If closest is found, return the flightphase and procedure
     if (closest) {
       const parsedProcedure = parseInt(closest.Procedure);
-      const procedure = Number.isInteger(parsedProcedure) ? parsedProcedure : closest.Procedure;
-      return {
-        FlightPhase: closest.FlightPhase,
-        Procedure: procedure,
-      };
+      closest.Procedure = Number.isInteger(parsedProcedure) ? parsedProcedure : closest.Procedure;
+
+      if(closest.FlightPhase == "Flight"){
+        closest.Procedure = normalizeFlightProcedureString(closest.Procedure);
+      }
+
+      return closest;
     }
 
     return null;
   }
 
-  async function fetchMissionLogNData(filePath) {
+  async function fetchData(filePath) {
     return d3.json(filePath)
     .then(data => data)
     .catch(error => {
       console.error('Error reading or parsing file:', error);
       throw error;
     });
+  }
+
+function clearDiv(divID){
+  const Div =  document.getElementById(divID);
+  if(Div){
+    Div.innerHTML = '';
+  }
+}
+
+function LoadWorkloadModule(){
+  
+  const targetDiv = document.getElementById('ideal-log-container-child');
+
+  if (targetDiv) {
+    // Create the main container div
+    const mainContainer = document.createElement('div');
+    mainContainer.id = "workload-module-main-container";
+
+    // mainContainer.style.height = '100%';
+    
+    // Create and append the data fields
+    fieldLabels.forEach(label => {
+      const dataField = document.createElement('div');
+      dataField.className = 'ideal-workload-datafield';
+    
+      const innerDiv = document.createElement('div');
+      innerDiv.classList.add('workload-module-datafield-container');
+    
+      const valueDiv = document.createElement('div');
+      valueDiv.classList.add('workload-module-datafield-inner-div');
+      const valueText = document.createElement('h1');
+      valueText.id = label[2];
+      valueText.style.margin = "auto";
+      valueText.textContent = 'N/A'; // Example value, you can set this dynamically
+      valueDiv.appendChild(valueText);
+    
+      const unitDiv = document.createElement('div');
+      unitDiv.classList.add('workload-module-datafield-inner-div');
+      const unitText = document.createElement('p');
+      unitText.style.marginBottom = "4px";
+      unitText.style.marginLeft = "2px";
+      unitText.textContent = label[1]; // Example unit, you can set this dynamically
+      unitDiv.appendChild(unitText);
+    
+      innerDiv.appendChild(valueDiv);
+      innerDiv.appendChild(unitDiv);
+    
+      dataField.appendChild(innerDiv);
+    
+      const strongElement = document.createElement('p');
+      strongElement.style.fontStyle = "italic";
+      strongElement.style.margin = "0px";
+      strongElement.textContent = label[0];
+      dataField.appendChild(strongElement);
+    
+      mainContainer.appendChild(dataField);
+    });
+
+    // Create the image container
+    const imageContainer = document.createElement('div');
+    imageContainer.style.width = "100%";
+    const imageElement = document.createElement('img');
+    imageElement.id = 'work-module-image';
+    // imageElement.src = 'data/images/preflight_1_chartView.png';
+    imageElement.src = '';
+    imageElement.style.width = "inherit";
+    imageContainer.appendChild(imageElement);
+
+    // Clear the existing content
+    targetDiv.innerHTML = '';
+
+    // Append the main container and image container to the target div
+    targetDiv.appendChild(mainContainer);
+    targetDiv.appendChild(imageContainer);
+  }
+
+  DefaultContainer = targetDiv.outerHTML;
+}
+
+function normalizeFlightProcedureString(input) {
+    // Convert the input to a string
+    let str = input.toString();
+
+    // Regular expression to match patterns with a prefix followed by digits
+    const prefixPattern = /^([a-zA-Z]+)-?(\d+)$/;
+
+    // Check if the input matches the pattern with a prefix
+    let match = str.match(prefixPattern);
+    if (match) {
+        // Extract the prefix and number parts
+        let prefix = match[1];
+        let number = match[2];
+
+        // Normalize the prefix to Title Case (e.g., 'Emer' or 'emer' to 'Emer')
+        prefix = prefix.charAt(0).toUpperCase() + prefix.slice(1).toLowerCase();
+
+        // Return the normalized string in the format 'Prefix-Number'
+        return `${prefix}-${number}`;
+    }
+
+    // If the input is just a number, return it as it is
+    if (!isNaN(str)) {
+        return str;
+    }
+
+    // If input doesn't match the expected patterns, return the original input
+    return input;
 }
